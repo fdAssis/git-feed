e206ed14fcdd203096ce5d5a7ff199833f6f53b2
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-10-26 22:33:12 -0300

    feat: add props in component

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -2,50 +2,36 @@ import { Avatar } from "../Avatar";
 import { Comment } from "../Comment";
 import styles from "./styles.module.css";
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.profile}>
-          <Avatar src="https://images.unsplash.com/photo-1508280756091-9bdd7ef1f463?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=40" />
+          <Avatar src={author.avatar_url} />
           <div className={styles.authorInfo}>
-            <strong>Jane Cooper</strong>
-            <span>Dev Front-End</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
         <time title="14 de Setembro às 12:00h" dateTime="2022-09-14 12:00">
-          Públicado há 1H
+          Publicado há 1H
         </time>
       </header>
-
       <div className={styles.content}>
-        <div>
-          <p>Fala dev ✨</p>
-          <p>
-            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-            no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
-            🚀
-          </p>
-          <p>
-            <a className={styles.sharedLink} href="">
-              jane.design/doctorcare
-            </a>
-          </p>
-        </div>
-
-        <div className={styles.hashtags}>
-          <p>
-            <a href="">#novoprojeto</a>
-          </p>
-          <p>
-            <a href="">#nlw</a>
-          </p>
-          <p>
-            <a href="">#rocketseat</a>
-          </p>
-        </div>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a className={styles.sharedLink} href="">
+                  {line.content}
+                </a>
+              </p>
+            );
+          }
+        })}
       </div>
-
       <form className={styles.comentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea placeholder="Escreva um comentário..." />
@@ -53,7 +39,6 @@ export function Post() {
           <button type="submit">Publicar</button>
         </footer>
       </form>
-
       <div className={styles.comments}>
         <Comment />
         <Comment />
