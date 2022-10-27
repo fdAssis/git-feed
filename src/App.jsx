f9d798d8bc570bd3c6eaacae7e9a5b3b7a027dbc
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-10-26 22:32:09 -0300

    feat: use data api

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,36 @@ import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
-import styles from './app.module.css';
+import styles from "./app.module.css";
+
+import { api } from "./service/api";
+import { useEffect, useState } from "react";
 export function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("/posts")
+      .then((response) => setPosts(response.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
-    <Header/>
+      <Header />
       <div className={styles.wrapper}>
         <Sidebar />
-        <Post />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.published_at}
+              />
+            );
+          })}
+        </main>
       </div>
     </>
   );
